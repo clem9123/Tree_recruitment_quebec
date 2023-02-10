d3edb072ddd834b58feb691ffc07fb52c87e24e9
Clementine de Montgolfier <clementine.de.montgolfier@ens.psl.eu>
2023-02-10 19:05:44 +0100

    work in progress

---
+++ script/Junk.R
@@ -0,0 +1,11 @@
+head(tree_mes_xy)
+head(sap_mes_xy)
+
+tree <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "dendro_arbres")
+zone_pe <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "classi_eco_pe")
+gaule <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "dendro_gaules")
+semis <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "station_semis")
+perturbation <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "pee_ori_sond")
+mesure <- st_read("data/PEP_GPKG/PEP.gpkg", layer = "placette_mes")
+
+head(zone_pe)
